419a0a02649c25fe87565d50ab2871552eb3c185
weissle <2016301500298@whu.edu.cn>
2022-07-12 00:36:44 +0800

    remove and change some keymap

--- lua/mappings/mappings.lua
+++ lua/mappings/mappings.lua
@@ -88,8 +88,4 @@ keymap("n", "<leader>dk", "<cmd>lua require'dapui'.eval()<cr>", opts)
 keymap("t", "jj", "<C-\\><C-n>", opts)
 
 --> smart split
-vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
-vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
-vim.keymap.set('n', '<leader>w', require('smart-splits').start_resize_mode)
+vim.keymap.set('n', '<leader>mw', require('smart-splits').start_resize_mode)
