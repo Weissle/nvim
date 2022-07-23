532619a772d9a98ccd8e7ee6716fd6baa91ed393
weissle <2016301500298@whu.edu.cn>
2022-07-24 00:44:32 +0800

    enter insert mode when goto term

--- lua/settings.lua
+++ lua/settings.lua
@@ -29,6 +29,17 @@ vim.api.nvim_create_autocmd('BufWritePost',{
 	end
 })
 
+vim.api.nvim_create_augroup('move-to-term',{clear=true})
+vim.api.nvim_create_autocmd('BufEnter',{
+	group = 'move-to-term',
+	callback = function ()
+		local file_name = vim.api.nvim_buf_get_name(0)
+		if string.find(file_name,'^term') then
+			vim.cmd('startinsert')
+		end
+	end
+})
+
 vim.api.nvim_create_autocmd({"BufReadPost"},{ callback = require('persistent-breakpoints.api').load_breakpoints })
 
 vim.defer_fn(function ()
