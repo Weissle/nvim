dcea9cdc47a778b56ccda3a550542f726215ea84
weissle <2016301500298@whu.edu.cn>
2022-09-11 14:16:00 +0800

    better

--- lua/plugins/setup/auto-session.lua
+++ lua/plugins/setup/auto-session.lua
@@ -9,16 +9,20 @@ local close_all_floating_wins = function()
 	end
 end
 
+local close_symbols_outline = function()
+	pcall(vim.cmd, "SymbolsOutlineClose")
+end
+
 M.setup = function(_)
 	local auto_session = require("auto-session")
 	-- NOTE: folds options is exclusive. Due to ufo is not loaded at startup.
 	vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal"
 	_G.close_all_floating_wins = close_all_floating_wins
-	_G.close_nvim_tree = close_nvim_tree
+	_G.close_symbols_outline = close_symbols_outline
 	auto_session.setup({
 		log_level = "error",
 		auto_session_suppress_dirs = { "~/" },
-		pre_save_cmds = { "NvimTreeClose", _G.close_all_floating_wins },
+		pre_save_cmds = { "NvimTreeClose", _G.close_all_floating_wins, _G.close_symbols_outline },
 	})
 end
 
