28adc9a85f38f4f227f048b39bf4f52252bc5931
weissle <2016301500298@whu.edu.cn>
2023-01-30 19:32:18 +0800

    use unicode icon.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,4 +11,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins_spec")
+require("lazy").setup("plugins_spec", {
+	ui = {
+		icons = {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
